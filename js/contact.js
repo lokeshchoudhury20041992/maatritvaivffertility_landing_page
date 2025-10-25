472e1fd4f284510f7183013973cf802f82e54375
lokeshchoudhury20041992 <lokeshchoudhury.s@gmail.com>
2025-10-25 14:39:12 +0530

    Update Supabase URL and ANON key in contact.js

--- js/contact.js
+++ js/contact.js
@@ -4,11 +4,9 @@
 // Note: This import may be redundant if the code from step 1 is used, but it's the correct
 // pattern for a true module environment. We will rely on the global assignment from Step 1.
 
-const SUPABASE_URL = "https://uuidrwjzdbqzgightlwc.supabase.co";
+const SUPABASE_URL = "https://gxtzhojshtcxtmqtobai.supabase.co";
 // Note: process.env.SUPABASE_KEY is for Node environments. We use your ANON key here.
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1aWRyd2p6ZGJxemdpZ2h0bHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNDQyNjAsImV4cCI6MjA3NDYyMDI2MH0.UJhYnG6GniyWvEZ9vt-wbCzHrWjiHB6Dw1dxBMEnj3E";
-
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4dHpob2pzaHRjeHRtcXRvYmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2NTI5NjgsImV4cCI6MjA3NTIyODk2OH0.vu3TynNWVUzbM66taJapM3bPDCb3PB4dpXh44oUydqg";
 // We use the global function created in index.html to initialize the client.
 console.log("Initializing Supabase client using module approach...");
 
